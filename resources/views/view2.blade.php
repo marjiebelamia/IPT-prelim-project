621da9f65beaceafd441a7732b5299fdf286134c
Your Name <you@example.com>
2021-09-20 20:20:15 +0800

    first commit

---
+++ resources/views/view2.blade.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Prelim Project</title>
+        <link rel="stylesheet" href="{{asset('css/bootstrap.min.css')}}">
+    </head>
+    <body>
+
+        <div  class="d-flex aligns-items-center justify-content-center mt-5">
+            <div class="badge bg-primary text-wrap">
+                <h5>Integrative Programming & Technology Prelim Project</h5>
+            </div>
+        </div>
+
+        <div>
+            <nav  class="d-flex align-items-center justify-content-center" style="height:200px">
+                <ul class="d-flex list-unstyled m-5">
+                    <li><a class="nav-link mt-5 text-secondary" href="{{url('/')}}">Page 1</a></li>
+                    <li><a class="nav-link mt-5 h5 success" href="{{url('/view2')}}">Page 2</a></li>
+                    <li><a class="nav-link mt-5 text-secondary" href="{{url('/view3')}}">Page 3</a></li>
+                </ul>
+            </nav>
+        </div>
+
+        <div class="container d-flex justify-content-center align-items-center">
+            <h5>Marjie Belamia BSIT-3</h5>
+        </div>
+
+        <link rel="stylesheet" href="{{asset('js/bootstrap.min.js')}}">
+    </body>
+</html>
